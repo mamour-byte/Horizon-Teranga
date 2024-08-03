030fc7f3a94e5b9f55445a633cbdf491b42e5dad
mamour fall <mamourf958@gmail.com>
2024-08-03 22:37:52 +0000

    changement

--- lib/Services/Database.dart
+++ lib/Services/Database.dart
@@ -33,6 +33,21 @@ class DatabaseService {
     );
   }
 
+  Future<AppUserData> userData(String userId) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+          .collection('User').doc(userId).get();
+      if (snapshot.exists) {
+        return _userFromSnapshot(snapshot);
+      } else {
+        throw Exception('Utilisateur non trouvé');
+      }
+    } catch (e) {
+      throw Exception('Erreur lors de la récupération des données : $e');
+    }
+  }
+
+
 
 
 
--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -1,162 +1,189 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  User? _user;
+  late Future<DocumentSnapshot> _userDocument;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = FirebaseAuth.instance.currentUser;
+    if (_user != null) {
+      _userDocument = FirebaseFirestore.instance
+          .collection('User')
+          .doc(_user!.uid)
+          .get();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 16),
+    return FutureBuilder<DocumentSnapshot>(
+      future: _userDocument,
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Profil')),
+            body: const Center(child: CircularProgressIndicator()),
+          );
+        }
 
+        if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Profil')),
+            body: const Center(child: Text('Erreur de chargement')),
+          );
+        }
 
-              const CircleAvatar(
-                radius: 60,
-                backgroundImage: NetworkImage(
-                  'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D',
-                ),
-              ),
-              const SizedBox(height: 16),
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Profil')),
+            body: const Center(child: Text('Aucun utilisateur trouvé')),
+          );
+        }
 
-              // Nom de l'utilisateur
-              const Text(
-                'Nom Utilisateur',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins',
-                ),
-              ),
-              const SizedBox(height: 8),
+        var userData = snapshot.data!.data() as Map<String, dynamic>;
 
-
-              const Text(
-                'email@example.com',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                  fontFamily: 'Poppins',
-                ),
-              ),
-              const SizedBox(height: 16),
-
-
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.phone, color: Colors.brown),
-                  SizedBox(width: 8),
-                  Text(
-                    '+221 123 456 789',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                      fontFamily: 'Poppins',
-                    ),
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 16),
+                const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D',
                   ),
-                ],
-              ),
-              const SizedBox(height: 16),
-
-              // Date de naissance
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
-                  Icon(Icons.cake, color: Colors.brown),
-                  SizedBox(width: 8),
-                  Text(
-                    '01 Janvier 1990',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-
-
-              ElevatedButton.icon(
-                onPressed: () {
-                  // Action pour modifier le profil
-                },
-                icon: const Icon(Icons.edit),
-                label: const Text('Modifier le profil'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.brown,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                  textStyle: const TextStyle(
-                    fontSize: 16,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  userData['NomUser'] ?? 'Nom inconnu',
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                     fontFamily: 'Poppins',
                   ),
                 ),
-              ),
-              const SizedBox(height: 24),
-
-              // Section Paramètres
-              _buildSettingsOption(
-                context,
-                icon: Icons.settings,
-                label: 'Paramètres du compte',
-                onTap: () {
-                  // Action pour accéder aux paramètres
-                },
-              ),
-              _buildSettingsOption(
-                context,
-                icon: Icons.lock,
-                label: 'Changer le mot de passe',
-                onTap: () {
-                  // Action pour changer le mot de passe
-                },
-              ),
-              _buildSettingsOption(
-                context,
-                icon: Icons.help_outline,
-                label: 'Support',
-                onTap: () {
-                  // Action pour accéder au support
-                },
-              ),
-              const SizedBox(height: 24),
-
-              // Bouton pour se déconnecter
-              OutlinedButton.icon(
-                onPressed: (){
-                  FirebaseAuth.instance.signOut() ;
-                },
-                icon: const Icon(Icons.logout),
-                label: const Text('Déconnexion'),
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(color: Colors.brown),
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                  textStyle: const TextStyle(
+                const SizedBox(height: 8),
+                Text(
+                  userData['EmailUser'] ?? 'Email inconnu',
+                  style: const TextStyle(
                     fontSize: 16,
+                    color: Colors.grey,
                     fontFamily: 'Poppins',
-                    color: Colors.brown,
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-            ],
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.phone, color: Colors.brown),
+                    const SizedBox(width: 8),
+                    Text(
+                      userData['TelephoneUser'] ?? 'Téléphone inconnu',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.cake, color: Colors.brown),
+                    const SizedBox(width: 8),
+                    Text(
+                      userData['DateUser'] ?? 'Not found',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Action pour modifier le profil
+                  },
+                  icon: const Icon(Icons.edit),
+                  label: const Text('Modifier le profil'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                _buildSettingsOption(
+                  context,
+                  icon: Icons.settings,
+                  label: 'Paramètres du compte',
+                  onTap: () {},
+                ),
+                _buildSettingsOption(
+                  context,
+                  icon: Icons.lock,
+                  label: 'Changer le mot de passe',
+                  onTap: () {},
+                ),
+                _buildSettingsOption(
+                  context,
+                  icon: Icons.help_outline,
+                  label: 'Support',
+                  onTap: () {},
+                ),
+                const SizedBox(height: 24),
+                OutlinedButton.icon(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  icon: const Icon(Icons.logout),
+                  label: const Text('Déconnexion'),
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: Colors.brown),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                      color: Colors.brown,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
-        )
+        );
+      },
     );
   }
 
-  Widget _buildSettingsOption(
-      BuildContext context, {
-        required IconData icon,
-        required String label,
-        required VoidCallback onTap,
-      }) {
+  Widget _buildSettingsOption(BuildContext context, {
+    required IconData icon,
+    required String label,
+    required VoidCallback onTap,
+  }) {
     return ListTile(
       leading: Icon(icon, color: Colors.brown),
       title: Text(
@@ -171,5 +198,3 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
-
-
